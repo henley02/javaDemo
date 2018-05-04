688bec4e53ef89374df4c34803caa580c5d0727a
henley02 <410451447@qq.com>
2018-05-04 17:56:45 +0800

    init

---
+++ sjcw/src/main/webapp/src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import VueResource from 'vue-resource';
+import 'common/stylus/index.styl';
+import mixins from './public/mixins/global.js';
+
+Vue.config.productionTip = false;
+Vue.mixin(mixins);
+Vue.use(VueResource);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {App}
+});
