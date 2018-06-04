d39e43e303f13673f7ef9181a63b6b6c40de5b94
henley02 <410451447@qq.com>
2018-06-04 16:29:35 +0800

    [+]add service、controller

---
+++ src/main/java/com/sjcw/handler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.sjcw.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jdd on 2018/6/4.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    private Map<String, Object> exceptionHandler(HttpServletRequest req, Exception e) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        modelMap.put("success", false);
+        modelMap.put("errMsg", e.getMessage());
+        return modelMap;
+    }
+}
